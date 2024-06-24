9df2e2c9bd4c9de28fc64c0bf6313b16c429a373
AleandroSm <l21021167@veracruz.tecnm.mx>
2024-06-23 21:18:33 -0600

    use .evn

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import {Map} from './components/Map'
 
 function App() {
 
+  const GEO_API_KEY = import.meta.env.VITE_API_KEY 
   const [data,setData] = useState(null)
   const [target,setTarget] = useState('')
   const getTarget = (e,setErr) => {
@@ -23,17 +24,21 @@ function App() {
 
 
   useEffect(() => {
-  fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=at_5go7XM6HjdaKTxHAXWRh4PTTrG14n&${validateIpAddress(target) ? `ipAddress=${target}` : validateDomain(target) ? `domain=${target}` : ""}`)
-  .then(res => res.json())
-  .then(data => setData(data))
-  console.log(data)
+    try {
+        fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=${GEO_API_KEY}&${validateIpAddress(target) ? `ipAddress=${target}` : validateDomain(target) ? `domain=${target}` : ""}`)
+        .then(res => res.json())
+        .then(data => setData(data))
+        console.log(data)
+    } catch (error) {
+      console.log(error)
+    }
 },[target])
   
   return (
-    <main className='h-screen relative'>
-    <div className='h-[20%] flex items-center justify-center flex-col gap-6'>
-      <h1 className="relative z-10 text-white font-bold text-3xl">IP Addres Tracker</h1>
-      <img src={top} alt="top" className='absolute top-0 object-cover w-full' />
+    <main className='h-screen relative sm:min-w-full'>
+    <div className='md:h-[20%]  flex items-center justify-center flex-col gap-6'>
+      <h1 className="relative z-10 text-white font-bold text-lg md:text-3xl ">IP Addres Tracker</h1>
+      <img src={top} alt="top" className='absolute top-0 object-cover w-full h-full' />
       <Search getTarget={getTarget} />
     </div>
       <Card ip={data?.ip} city={data?.location?.city} region={data?.location?.region} asn={data?.as?.asn} timeZone={data?.location?.timezone} isp={data?.isp} />
